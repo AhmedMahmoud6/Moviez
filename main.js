1505e205ecec1b6185e29597353ca2c2e815dc88
Ahmed Mahmoud <ahmedmahmoudd626@gmail.com>
2025-06-26 05:24:00 +0300

    navbar design

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
 import { getData, getCustomData } from "./functions.js";
 
-const APIKEY = "9cca2fe3162fa3d7db2b1762e9779b1d";
+// const APIKEY = "9cca2fe3162fa3d7db2b1762e9779b1d";
 
-let getPopular = `https://api.themoviedb.org/3/movie/popular?api_key=${APIKEY}&language=en-US`;
-let searchQuery = "";
-let searchMovies = `https://api.themoviedb.org/3/search/movie?api_key=${APIKEY}&query=${searchQuery}`;
-let selectedGenre = 0;
-let discoverMovies = `https://api.themoviedb.org/3/discover/movie?api_key=${APIKEY}&with_genres=${selectedGenre}`;
-let upcomingMovie = `https://api.themoviedb.org/3/movie/upcoming?api_key=${APIKEY}&language=en-US`;
+// let getPopular = `https://api.themoviedb.org/3/movie/popular?api_key=${APIKEY}&language=en-US`;
+// let searchQuery = "";
+// let searchMovies = `https://api.themoviedb.org/3/search/movie?api_key=${APIKEY}&query=${searchQuery}`;
+// let selectedGenre = 0;
+// let discoverMovies = `https://api.themoviedb.org/3/discover/movie?api_key=${APIKEY}&with_genres=${selectedGenre}`;
+// let upcomingMovie = `https://api.themoviedb.org/3/movie/upcoming?api_key=${APIKEY}&language=en-US`;
 
-getData(getPopular).then((data) => {
-  data.results.forEach((movie) => {
-    console.log(movie);
-  });
-});
+// getData(getPopular).then((data) => {
+//   data.results.forEach((movie) => {
+//     console.log(movie);
+//   });
+// });
