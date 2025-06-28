b6a6b59b1e3dd795288ab18791497269df269865
Ahmed Mahmoud <ahmedmahmoudd626@gmail.com>
2025-06-28 23:37:07 +0300

    movie desc,cast and simillar design

--- swiper.js
+++ swiper.js
@@ -1,39 +1,73 @@
-const swiper = new Swiper(".swiper", {
+document.querySelectorAll(".swiper").forEach((el) => {
+  new Swiper(el, {
+    slidesPerView: 4,
+    slidesPerGroup: 4,
+    rewind: true,
+    watchOverflow: true,
+    spaceBetween: 30,
+    pagination: {
+      el: el.querySelector(".swiper-pagination"),
+      clickable: true,
+    },
+    navigation: {
+      nextEl: el.querySelector(".swiper-button-next"),
+      prevEl: el.querySelector(".swiper-button-prev"),
+    },
+    breakpoints: {
+      1280: {
+        slidesPerView: 4,
+        slidesPerGroup: 4,
+      },
+      1024: {
+        slidesPerView: 3,
+        slidesPerGroup: 3,
+      },
+      768: {
+        slidesPerView: 2,
+        slidesPerGroup: 2,
+      },
+      450: {
+        slidesPerView: 2,
+        slidesPerGroup: 2,
+      },
+      0: {
+        slidesPerView: 1,
+        slidesPerGroup: 1,
+      },
+    },
+  });
+});
+
+const secondSwiper = new Swiper(".second-swiper", {
   // loop: true,
   slidesPerView: 4,
   slidesPerGroup: 4,
   rewind: true,
   watchOverflow: true,
-  spaceBetween: 30,
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
+  spaceBetween: 20,
+
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
 
   breakpoints: {
-    1280: {
+    1536: {
       slidesPerView: 4,
       slidesPerGroup: 4,
     },
-    1024: {
+    1000: {
+      slidesPerView: 5,
+      slidesPerGroup: 5,
+    },
+    440: {
       slidesPerView: 3,
       slidesPerGroup: 3,
     },
-    768: {
-      slidesPerView: 2,
-      slidesPerGroup: 2,
-    },
-    450: {
-      slidesPerView: 2,
-      slidesPerGroup: 2,
-    },
+
     0: {
-      slidesPerView: 1,
-      slidesPerGroup: 1,
+      slidesPerView: 2,
+      slidesPerGroup: 2,
     },
   },
 });
